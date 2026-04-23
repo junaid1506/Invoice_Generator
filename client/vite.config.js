cc343661c2e1e1653dc8ca893c68f0623e000cce
junaid1506 <junnu2757@gmail.com>
2026-04-23 10:29:29 +0530

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://invoice-generator-s16o.onrender.com/",
+        target: "https://invoice-generator-s16o.onrender.com",
         changeOrigin: true,
       },
     },
